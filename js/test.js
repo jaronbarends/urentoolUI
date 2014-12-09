cbd2afd73352248f5156f40daa75471fe4752f0f
jaronbarends <ik@jaron.nl>
2014-12-09 09:45:43 +0100

    commit just to be sure

---
+++ js/test.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+
+// @name          testje3
+
+// @namespace     http://www.webmonkey.com
+
+// @description   A basic example of Greasemonkey that causes an alert at each page load.
+
+// @include https://uren.netwerkgroep.nl/*
+// @exclude https://uren.netwerkgroep.nl/leeg.html
+
+// ==/UserScript==
+
+
+alert('jQuery:"'+typeof(jQuery));
+
+
